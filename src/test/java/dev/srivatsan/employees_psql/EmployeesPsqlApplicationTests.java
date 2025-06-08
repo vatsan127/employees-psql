5e44d64e5db4dd66c96964af995f98236d660d55
vatsan127 <vatsan127@gmail.com>
2025-06-08 23:46:09 +0530

    first commit

---
+++ src/test/java/dev/srivatsan/employees_psql/EmployeesPsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.srivatsan.employees_psql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesPsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
